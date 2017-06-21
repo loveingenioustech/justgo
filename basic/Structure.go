7c0d2e770c25f1abfc7b9eb0dfe0611579db765f
Robin <loveingenioustech@gmail.com>
2017-06-21 22:25:20 +0800

    basic, 9982

---
+++ basic/Structure.go
@@ -0,0 +1,28 @@
+package main
+
+// 导入其他的包
+import "fmt"
+
+// 常量的定义
+const PI = 3.14
+
+// 全局变量
+var golbal = "test"
+
+// 一般类型声明
+type newType int
+
+// 结构的声明
+type s1 struct {
+
+}
+
+// 接口的声明
+type golang interface {
+
+}
+
+// main函数入口
+func main() {
+	fmt.Println("Hello")
+}
