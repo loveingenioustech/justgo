0a0393464dbed21c2e76e30c27ed2413ecbf1503
Robin Long <loveingenioustech@gmail.com>
2017-07-03 16:36:36 +0800

    form, 9974

---
+++ form/FormDemo2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"html/template"
+	"crypto/md5"
+	"io"
+	"time"
+	"strconv"
+)
+
+
+func login2(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("form/login2.html")
+		t.Execute(w, token)
+	} else {
+		//请求的是登陆数据，那么执行登陆的逻辑判断
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			//验证token的合法性
+		} else {
+			//不存在token报错
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+	}
+}
+
+func main() {
+	http.HandleFunc("/login2", login2)         //设置访问的路由
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
